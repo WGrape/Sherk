dc2ab5e3c3953f421e4969b78dd0e0bac0bbffda
PC\Administrator <15239928381@163.com>
2018-10-22 11:14:30 +0800

    no message

--- Sherk.c
+++ Sherk.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
-#include "src/launcher/client.launcher.c"
-#include "src/launcher/server.launcher.c"
+//#include "src/launcher/client.launcher.c"
+//#include "src/launcher/server.launcher.c"
 
 
 // 主程序
-int main() {
+int main(int argc, char *argv[]) {
+
+    int i;
+
+    printf("the count of args is %d \n", argc-1);
+
+    for (i = 2; i <= argc; i++) {
+
+        printf("%s ", argv[i-1]);
+    }
+
+    printf("sherk > ");
+
+    getchar();
 
     return 0;
 }
