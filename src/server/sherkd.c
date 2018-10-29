ac1201b31b5d5a7e006b310e95806226de1b0ff6
PC\Administrator <15239928381@163.com>
2018-10-29 17:42:17 +0800

    no message

--- src/server/sherkd.c
+++ src/server/sherkd.c
@@ -1,59 +1,70 @@
+/*************************************************************************
+    > File Name: sem.c
+    > Author: couldtt(fyby)
+    > Mail: fuyunbiyi@gmail.com
+    > Created Time: 2013年12月15日 星期日 19时25分08秒
+ ************************************************************************/
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
+#define MAXSIZE 10
 
-void print_message_function (void *ptr);
+int stack[MAXSIZE]; // 生产队列, 存放生产的东西
 
-int main()
-{
+sem_t sem; // semaphore 信号量
 
-    // 被等待线程的返回值
-    void *retval; // void函数的指针
+// 生产东西
+void provide_data(void) {
 
-    // 定义2个线程
-    pthread_t thread1, thread2;
+    for (int i=0; i< MAXSIZE; i++) {
 
-    char *message1 = "thread1";
-    char *message2 = "thread2";
+        stack[i] = i;
 
-    // 线程1和线程2的创建状态
-    int create_status_thread1 = pthread_create(&thread1, NULL, (void *)&print_message_function, (void *) message1);
-    int create_status_thread2 = pthread_create(&thread2, NULL, (void *)&print_message_function, (void *) message2);
-
-    // 线程创建成功，返回0,失败返回失败号
-    if (create_status_thread1 != 0) {
-
-        printf("线程1创建失败\n");
+        // sem_post是给信号量的值加上一个“1”，
+        // 它是一个“原子操作”,即同时对同一个信号量做加“1”操作的两个线程是不会冲突的；而同时对同一个文件进行读和写操作的两个程序就有可能会引起冲突。
+        sem_post(&sem); //为信号量加1
     }
-
-    if (create_status_thread2 != 0) {
-        printf("线程2创建失败\n");
-    }
-
-    // 主线程以阻塞的方式等待线程thread1的结束
-    if(pthread_join(thread1, &retval) != 0){
-
-        printf("the main thread pthread_join thread1 with a error\n");
-    }
-    printf("thread1 return value(retval) is %d\n", (int)retval);
-    printf("thread1 end\n");
-
-
-
-    tmp2 = pthread_join(thread1, &retval);
-    printf("thread2 return value(retval) is %d\n", (int)retval);
-    printf("thread2 return value(tmp) is %d\n", tmp1);
-    if (tmp2 != 0) {
-        printf("cannot join with thread2\n");
-    }
-
-    printf("thread2 end\n");
 }
 
-void print_message_function( void *ptr ) {
-    int i = 0;
-    for (i; i<5; i++) {
-        printf("%s:%d\n", (char *)ptr, i);
+// 消费
+void produce_data(void) {
+
+    int i=0;
+
+    while(i < MAXSIZE) {
+
+        // sem_wait是一个函数，也是一个原子操作，
+        // 它的作用是从信号量的值减去一个“1”，但它永远会先等待该信号量为一个非零值才开始做减法。
+        // 也就是说，如果你对一个值为2的信号量调用sem_wait()，线程将会继续执行，将信号量的值将减到1。
+        sem_wait(&sem);
+
+        printf("%d X %d = %d\n", stack[i], stack[i], stack[i]*stack[i]);
+
+        sleep(1); // 可加可不加，加上的话，可以清楚的看出过程
+
+        ++i;
     }
+}
+
+int main(void) {
+
+    // 定义生产者线程和消费者线程
+    pthread_t provider, producer;
+
+    sem_init(&sem, 0, 0); //信号量初始化
+
+    // 创建生产者线程和消费者线程
+    pthread_create(&provider, NULL, (void *)produce_data, NULL);
+    pthread_create(&producer, NULL, (void *)provide_data, NULL);
+
+    // 等待生产者和消费者线程的结束
+    pthread_join(provider, NULL);
+    pthread_join(producer, NULL);
+
+    sem_destroy(&sem); //销毁信号量
+
+    return 0;
 }
