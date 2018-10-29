10064ccc6c82f066d1c2c0db434088bdf4bb743c
PC\Administrator <15239928381@163.com>
2018-10-29 15:55:48 +0800

    no message

--- src/client/sherk.c
+++ src/client/sherk.c
@@ -7,18 +7,20 @@ int main(int argc, char argv[]) {
     // 登录
     if(RES_OK != command_login_success(argc, argv)){
 
-        // 登录失败
-        return 0;
+        // 客户端结束 - 登录失败
+        return 1;
     }
 
     // 进入sql交互环境中
     command_enter_sql_interactive_env();
 
     // 退出
-    if(!command_logout_success()){
+    if(RES_OK != command_logout_success()){
 
-        // 退出失败
+        // 客户端结束 - 退出失败
+        return 1;
     }
 
+    // 客户端正常结束
     return 0;
 }
