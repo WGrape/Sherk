be10ef2488b5f3974fcb83966581f3c7b42254d5
Lvsi-China <15239928381@163.com>
2019-05-11 22:49:07 +0800

    设计开发中2

--- src/server/build.sh
+++ src/server/build.sh
@@ -1,3 +1,5 @@
 /Applications/CLion.app/Contents/bin/cmake/mac/bin/cmake --build /Users/yimutian/Lvsi/github/Sherk/src/server/cmake-build-debug --target sherkd -- -j 2
 
 cp -i ./cmake-build-debug/sherkd ./bin/
+
+ln -s /Users/yimutian/Lvsi/github/Sherk/src/server/SherkLaunch/src/module/adapter/sherk.conf  /Users/yimutian/sherk/sherk.conf
