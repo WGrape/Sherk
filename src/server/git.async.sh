f73243d42c9d70b6ee5381f2a0c4bae488f4d19b
Lvsi-China <15239928381@163.com>
2019-05-18 14:53:03 +0800

    random_8531

---
+++ src/server/git.async.sh
@@ -0,0 +1,21 @@
+echo "开始同步";
+
+cd SherkEngine; git add .; git commit -m 'random_'$RANDOM; git push origin; echo "SherkEngine同步到GitHub成功"; cd ..
+
+cd SherkLaunch; git add .; git commit -m 'random_'$RANDOM; git push origin; echo "SherkLaunch同步到GitHub成功"; cd ..
+
+cd SherkLink; git add .; git commit -m 'random_'$RANDOM; git push origin; echo "SherkLink同步到GitHub成功"; cd ..
+
+cd SherkMechanism; git add .; git commit -m 'random_'$RANDOM; git push origin; echo "SherkMechanism同步到GitHub成功"; cd ..
+
+cd SherkSQL; git add .; git commit -m 'random_'$RANDOM; git push origin; echo "SherkSQL同步到GitHub成功"; cd ..
+
+cd SherkStorage; git add .; git commit -m 'random_'$RANDOM; git push origin; echo "SherkStorage同步到GitHub成功"; cd ..
+
+cd SherkSupport; git add .; git commit -m 'random_'$RANDOM; git push origin; echo "SherkSupport同步到GitHub成功"; cd ..
+
+cd SherkSystem; git add .; git commit -m 'random_'$RANDOM; git push origin; echo "SherkSystem同步到GitHub成功"; cd ..
+
+git add .; git commit -m 'random_'$RANDOM; git push origin; echo "Sherk同步到GitHub成功"
+
+echo "同步结束"
