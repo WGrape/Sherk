08f371b02dfb54cac9932a1172482edf2d3c725d
Lvsi-China <15239928381@163.com>
2019-05-19 16:02:00 +0800

    random_17340

--- src/server/sherkd.c
+++ src/server/sherkd.c
@@ -4,16 +4,15 @@
 #include <stdlib.h>
 #include <SherkSupport/src/module/table_traverser/table_traverser.h>
 #include <SherkSupport/src/include/const/core.h>
+#include <SherkSupport/src/module/loyal_serializer/loyal_serializer.h>
 
 int main() {
 
-//    sherkLaunch_start();
+    // sherkLaunch_start();
 
-    Struct_Table *struct_table_pointer = table_traverser_simulate_a_table_demo("test_table", 1,
+    Struct_Table *struct_table_pointer = table_traverser_simulate_a_table_demo("test_database", "test_table", 1,
                                                                                DATABASE_SUPPORT_ENGINE_TYPE_INNODB,
                                                                                DATABASE_SUPPORT_CHARSET_TYPE_UTF8);
-
-
 
     return 0;
 }
