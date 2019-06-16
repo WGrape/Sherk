59919073839166aa71ff6c230edea0b94093664e
Lvsi-China <15239928381@163.com>
2019-06-16 16:34:34 +0800

    random_18863

---
+++ docs/manual/README.md
@@ -0,0 +1,9 @@
+## Sherk 文档
+
+
+#### 项目目录
+
+```
+src/client, src/cluster, src/language_connector, src/server  这4个目录都是单独的进程目录，即每个目录都是以进程方式运行。
+```
+
--- src/client/module/encrypter/encrypter.c
+++ src/client/module/encrypter/encrypter.c
@@ -9,7 +9,7 @@ the merchantability of this software or the suitability of this
 software for any particular purpose. It is provided "as is"
 without express or implied warranty of any kind.
 These notices must be retained in any copies of any part of this
-documentation and/or software.
+manual and/or software.
  */
 
 /* The following makes MD default to MD5 if it has not already been
